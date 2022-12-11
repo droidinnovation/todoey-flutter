ab812d47da06881882de09086e1c5df6dfa002f9
VanDangDung <droidinnovation88@gmail.com>
2022-12-11 17:02:22 +0700

    Init code

---
+++ android/app/src/main/kotlin/com/dungdangvan/todoey_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dungdangvan.todoey_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 30.0,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {},
+              style:
+                  TextButton.styleFrom(backgroundColor: Colors.lightBlueAccent),
+              child: const Text(
+                'Add',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
